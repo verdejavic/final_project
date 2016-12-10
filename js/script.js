11bea98ca12ab430cb24aa94d8e9796d5b0bc53e
verdejavic <verdejavic@ufl.edu>
2016-12-09 23:59:52 -0500

    week 2&3

--- js/script.js
+++ js/script.js
@@ -1,25 +1,36 @@
-var mapboxAccessToken =  'pk.eyJ1IjoidmVyZGVqYXZpYyIsImEiOiJjaXZvNjV1eWQwMTR5MnpwY2cwc3ZobXN4In0.yNm14IMV2WWg4aQVVrQzHA';
-
 var map = L.map('demoMap').setView([37.8, -96], 4);
 
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=' + mapboxAccessToken, {
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoidmVyZGVqYXZpYyIsImEiOiJjaXZvNjV1eWQwMTR5MnpwY2cwc3ZobXN4In0.yNm14IMV2WWg4aQVVrQzHA', {
     id: 'verdejavic.278g414a',
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
    maxZoom: 18,
 }).addTo(map);
 
-L.geoJson(statesData).addTo(map);
+var info = L.control();
 
+info.onAdd = function (map) {
+  this._div = L.DomUtil.create('div', 'info');
+  this.update();
+  return this._div;
+};
 
-function getColor(d) {
-    return d > 105000 ? '#ffffcc' :
-           d > 90000  ? '#c7e9b4' :
-           d > 75000  ? '#7fcdbb' :
-           d > 60000  ? '#41b6c4' :
-           d > 45000  ? '#1d91c0' :
-           d > 30000  ? '#225ea8' :
-           d > 15000  ? '#0c2c84' :
-                      '#FFEDA0';
+info.update = function (props) {
+  this._div.innerHTML = '<h4>New Cancer Cases in the US</h4>' +  (props ?
+    '<b>' + props.name + '</b><br />' + props.density + ' cases'
+    : 'Hover over a state');
+};
+
+info.addTo(map);
+
+function getColor(density) {
+    return density > 105000 ? '#084594' :
+           density > 90000  ? '#2171b5' :
+           density > 75000  ? '#4292c6' :
+           density > 60000  ? '#6baed6' :
+           density > 45000  ? '#9ecae1' :
+           density > 30000  ? '#c6dbef' :
+           density > 15000  ? '#deebf7' :
+                      '#f7fbff';
 }
 
 function style(feature) {
@@ -33,4 +44,86 @@ function style(feature) {
     };
 }
 
-L.geoJson(statesData, {style: style}).addTo(map);
+function highlightFeature(e) {
+		var layer = e.target;
+
+		layer.setStyle({
+			weight: 5,
+			color: '#666',
+			dashArray: '',
+			fillOpacity: 0.7
+		});
+
+		if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+			layer.bringToFront();
+		}
+
+		info.update(layer.feature.properties);
+	}
+
+	var geojson;
+
+	function resetHighlight(e) {
+		geojson.resetStyle(e.target);
+		info.update();
+	}
+
+	function zoomToFeature(e) {
+		map.fitBounds(e.target.getBounds());
+	}
+
+	function onEachFeature(feature, layer) {
+		layer.on({
+			mouseover: highlightFeature,
+			mouseout: resetHighlight,
+			click: zoomToFeature
+		});
+	}
+
+	geojson = L.geoJson(statesData, {
+		style: style,
+		onEachFeature: onEachFeature
+	}).addTo(map);
+
+	map.attributionControl.addAttribution('Data from &copy; <a href="http://www.cdc.gov/cancer/dcpc/data/state.htm">Centers for Disease Control and Prevention</a>');
+
+
+var legend = L.control({position: 'bottomright'});
+
+legend.onAdd = function (map) {
+
+  var div = L.DomUtil.create('div', 'info legend'),
+    grades = [0, 15000, 30000, 45000, 60000, 75000, 90000, 105000],
+    labels = [],
+    from, to;
+
+  for (var i = 0; i < grades.length; i++) {
+    from = grades[i];
+    to = grades[i + 1];
+
+    labels.push(
+      '<i style="background:' + getColor(from + 1) + '"></i> ' +
+      from + (to ? '&ndash;' + to : '+'));
+  }
+
+  div.innerHTML = labels.join('<br>');
+  return div;
+};
+
+legend.addTo(map);
+
+//trialsMap
+
+var map = L.map('trialsMap').setView([26.82, 30.8], 1.5);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoidmVyZGVqYXZpYyIsImEiOiJjaXZvNjV1eWQwMTR5MnpwY2cwc3ZobXN4In0.yNm14IMV2WWg4aQVVrQzHA', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18,
+    id: 'verdejavic.278g414a',
+}).addTo(map);
+
+for ( var i=0; i < clinicalTrials.length; i++) {
+  L.marker( [clinicalTrials[i].latitude, clinicalTrials[i].longitude] )
+  .bindPopup( '<h5>' + clinicalTrials[i].trialname + '</h5>' +  '</h6>' + clinicalTrials[i].location + '</h6>' + '<p>' + 'Contact: '  + '<br>' + clinicalTrials[i].contactname  + '<br>' + clinicalTrials[i].contacttitle  + '<br>' + clinicalTrials[i].contactcompany  + '<br>' + clinicalTrials[i].contactemail + '<br>' + 'ClinicalTrials.gov Identifier: ' + clinicalTrials[i].identifier + '</p>' )
+  .addTo(map);
+}
